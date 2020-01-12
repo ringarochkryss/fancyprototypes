305ec7273b0019d62617b8e28418eae5aae37f31
ringarochkryss <petra@mellbrand.com>
2020-01-12 20:16:14 +0100

    Edit settings to display html  properly

--- welcomeapp/views.py
+++ welcomeapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render   # Added for this step
 
 def index(request):
-    return HttpResponse("Hello, Petra!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "welcomeapp/index.html",  # Relative path from the 'templates' folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
